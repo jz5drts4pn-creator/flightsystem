51c44b23c80f86140a19cbc256e408f4161864ff
GAR MALEK <jz5drts4pn@privaterelay.appleid.com>
2026-04-01 23:45:44 +0300

    Add login page and auth urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
 from . import views
 
-
-
 urlpatterns = [
     path('', views.home, name='home'),
     path('flights/', views.flight_list, name='flight_list'),
-    path('flights/book/<int:flight_id>/', views.book_flight, name='book_flight'),
+    path('book/<int:flight_id>/', views.book_flight, name='book_flight'),
     path('my-bookings/', views.my_bookings, name='my_bookings'),
-    path('bookings/cancel/<int:booking_id>/', views.cancel_booking, name='cancel_booking'),
-    
+    path('cancel/<int:booking_id>/', views.cancel_booking, name='cancel_booking'),
 ]
--- flightproject/urls.py
+++ flightproject/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from accounts.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),  # 👈 login/logout/password
-    path('', home, name='home'),
+    path('accounts/', include('django.contrib.auth.urls')),  # login/logout templates
+    path('accounts/', include('accounts.urls')),            # your app routes
+    path('', include('accounts.urls')),                     # homepage at root
 ]
